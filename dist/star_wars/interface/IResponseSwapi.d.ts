d350a28b6aefe18abe12b0cc25fe23bd60259179
EllenBecker <ellenbeckerdc@gmail.com>
2023-03-27 02:16:49 -0300

    Retirando pasta dist do GitHub

--- dist/star_wars/interface/IResponseSwapi.d.ts
+++
@@ -1,7 +0,0 @@
-import { IStarWars } from "./IStar_wars";
-export interface IResponseSwapi {
-    count: number;
-    next: string;
-    previous: string;
-    results: IStarWars[];
-}
--- dist/star_wars/interface/IReturnParameters.d.ts
+++
@@ -1,5 +0,0 @@
-import { IStarWars } from "./IStar_wars";
-export interface IReturnParameters {
-    items: IStarWars[];
-    totalPages: number;
-}
